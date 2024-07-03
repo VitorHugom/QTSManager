0b2cfed889a56dbfae435e4022b6f338ca434ef4
Vitor Hugo Moreira Souza <vitor.mor2015@gmail.com>
2024-07-02 21:48:43 -0300

    Atualização para vincullação de professores e cursos

--- src/app/components/professores-detail/professores-detail.component.ts
+++ src/app/components/professores-detail/professores-detail.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { ProfessorsService, Professor, Discipline, DayOfWeek } from '../../services/professor.service';
+import { ProfessorsService, Professor, Discipline, DayOfWeek, Course } from '../../services/professor.service';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
@@ -12,7 +12,7 @@ import { FormsModule } from '@angular/forms';
   styleUrls: ['./professores-detail.component.scss']
 })
 export class ProfessorDetailComponent implements OnInit {
-  professor: Professor = { id: 0, nome_professor: '', disciplines: [], daysOfWeek: [] };
+  professor: Professor = { id: 0, nome_professor: '', disciplines: [], daysOfWeek: [], courses: [] };
   isNew: boolean = true;
   allDisciplines: Discipline[] = [];
   linkedDisciplines: Discipline[] = [];
@@ -20,6 +20,9 @@ export class ProfessorDetailComponent implements OnInit {
   allDaysOfWeek: DayOfWeek[] = [];
   linkedDaysOfWeek: DayOfWeek[] = [];
   availableDaysOfWeek: DayOfWeek[] = [];
+  allCourses: Course[] = [];
+  linkedCourses: Course[] = [];
+  availableCourses: Course[] = [];
 
   constructor(
     private professorsService: ProfessorsService,
@@ -35,12 +38,15 @@ export class ProfessorDetailComponent implements OnInit {
         this.professor = data;
         this.linkedDisciplines = data.disciplines;
         this.linkedDaysOfWeek = data.daysOfWeek;
+        this.linkedCourses = data.courses;
         this.loadDisciplines();
         this.loadDaysOfWeek();
+        this.loadCourses();
       });
     } else {
       this.loadDisciplines();
       this.loadDaysOfWeek();
+      this.loadCourses();
     }
   }
 
@@ -58,6 +64,13 @@ export class ProfessorDetailComponent implements OnInit {
     });
   }
 
+  loadCourses(): void {
+    this.professorsService.getCourses().subscribe((courses: Course[]) => {
+      this.allCourses = courses;
+      this.filterAvailableCourses();
+    });
+  }
+
   filterAvailableDisciplines(): void {
     const linkedIds = this.linkedDisciplines.map(d => d.id);
     this.availableDisciplines = this.allDisciplines.filter(d => !linkedIds.includes(d.id));
@@ -66,6 +79,11 @@ export class ProfessorDetailComponent implements OnInit {
   filterAvailableDaysOfWeek(): void {
     const linkedIds = this.linkedDaysOfWeek.map(d => d.id);
     this.availableDaysOfWeek = this.allDaysOfWeek.filter(d => !linkedIds.includes(d.id));
+  }
+
+  filterAvailableCourses(): void {
+    const linkedIds = this.linkedCourses.map(c => c.id);
+    this.availableCourses = this.allCourses.filter(c => !linkedIds.includes(c.id));
   }
 
   linkDiscipline(discipline: Discipline): void {
@@ -88,13 +106,25 @@ export class ProfessorDetailComponent implements OnInit {
     this.filterAvailableDaysOfWeek();
   }
 
+  linkCourse(course: Course): void {
+    this.linkedCourses.push(course);
+    this.filterAvailableCourses();
+  }
+
+  unlinkCourse(course: Course): void {
+    this.linkedCourses = this.linkedCourses.filter(c => c.id !== course.id);
+    this.filterAvailableCourses();
+  }
+
   onSave(): void {
     const disciplineIds = this.linkedDisciplines.map(d => d.id);
     const daysOfWeekIds = this.linkedDaysOfWeek.map(d => d.id);
+    const coursesIds = this.linkedCourses.map(c => c.id);
     const professorData = {
       ...this.professor,
       disciplineIds,
-      daysOfWeekIds
+      daysOfWeekIds,
+      coursesIds
     };
 
     if (this.isNew) {
--- src/app/services/professor.service.ts
+++ src/app/services/professor.service.ts
@@ -15,11 +15,18 @@ export interface DayOfWeek {
   status: boolean;
 }
 
+export interface Course {
+  id: number;
+  nome_curso: string;
+  disciplines: Discipline[];
+}
+
 export interface Professor {
   id: number;
   nome_professor: string;
   disciplines: Discipline[];
   daysOfWeek: DayOfWeek[];
+  courses: Course[];
 }
 
 @Injectable({
@@ -29,6 +36,7 @@ export class ProfessorsService {
   private apiUrl = `${environment.apiUrl}/professors`;
   private disciplinesUrl = `${environment.apiUrl}/disciplines`;
   private daysOfWeekUrl = `${environment.apiUrl}/days_of_week`;
+  private coursesUrl = `${environment.apiUrl}/courses`;
 
   constructor(private http: HttpClient) {}
 
@@ -40,11 +48,11 @@ export class ProfessorsService {
     return this.http.get<Professor>(`${this.apiUrl}/${id}`);
   }
 
-  addProfessor(professor: Partial<Professor> & { disciplineIds: number[], daysOfWeekIds: number[] }): Observable<Professor> {
+  addProfessor(professor: Partial<Professor> & { disciplineIds: number[], daysOfWeekIds: number[], coursesIds: number[] }): Observable<Professor> {
     return this.http.post<Professor>(this.apiUrl, professor);
   }
 
-  updateProfessor(professor: Partial<Professor> & { disciplineIds: number[], daysOfWeekIds: number[] }): Observable<Professor> {
+  updateProfessor(professor: Partial<Professor> & { disciplineIds: number[], daysOfWeekIds: number[], coursesIds: number[] }): Observable<Professor> {
     return this.http.put<Professor>(`${this.apiUrl}/${professor.id}`, professor);
   }
 
@@ -58,5 +66,9 @@ export class ProfessorsService {
 
   getDaysOfWeek(): Observable<DayOfWeek[]> {
     return this.http.get<DayOfWeek[]>(this.daysOfWeekUrl);
+  }
+
+  getCourses(): Observable<Course[]> {
+    return this.http.get<Course[]>(this.coursesUrl);
   }
 }
